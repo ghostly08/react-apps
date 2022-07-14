7f9491c8d6bd1c2bee8b18440c5d75139e829283
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-14 10:25:35 +0200

    using fragment to render varoius element in the root

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,6 +1,10 @@
 
 export const FirstApp = () => {
   return (
-    <h1>Creating a new component on First App!!</h1>
-    );
+  <>
+    <h1>New component on First App!!</h1>
+    <p>Creating a new component in the app and learning fragment to render various elements in the same level or next to each others in the html</p>
+    <p>Fragment behaives like a "div" but dont breaks the html structure</p>
+  </>
+);
 }
