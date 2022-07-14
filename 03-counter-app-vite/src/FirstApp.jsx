ef44a09665452cc02b291730a1a70bfea4eee12a
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-14 13:49:22 +0200

    Different ways to get data and print it in the DOM

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,26 @@
 
+// ::::::::: Getting data from a function ::::::: //
+const name = (a) => {
+
+    return a;
+  };
+
+// ::::::::: Getting data from an Object :::::::: //
+const message = {
+  title: 'Printing variables on HTML',
+  message: 'Inside of curly braces we can render any JavaScript element but not Javascript objects. So for that, we have to acces to each elemet in the object or turning into JSON stringlify',
+}
+
+// :::::::: Exportable Component :::::::::: //
+
 export const FirstApp = () => {
   return (
   <>
-    <h1>New component on First App!!</h1>
+    <h1> Hello, { name('Steven') }!</h1>
+    <h1>{ message.title }</h1>
+    <p>{ message.message }</p>
+    <code>{ JSON.stringify(message) }</code>
+    <h3>New component on First App!!</h3>
     <p>Creating a new component in the app and learning fragment to render various elements in the same level or next to each others in the html</p>
     <p>Fragment behaives like a "div" but dont breaks the html structure</p>
   </>
