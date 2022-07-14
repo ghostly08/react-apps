b5b8041e5690d3fc2fd380c8183423aa1b72c9e3
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-14 09:56:35 +0200

    creating a new component in counter app

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,6 @@
+
+export const FirstApp = () => {
+  return (
+    <h1>Creating a new component on First App!!</h1>
+    );
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 
 import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render (
   <React.StrictMode>
     <HelloWorldApp/> {/*Hago l allamada a la function App()*/}
+    <FirstApp/>
   </React.StrictMode>
 );
