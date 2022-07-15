0bdc2c08431e05f006ce9a70886f68f9b429d124
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-15 13:18:16 +0200

    events - event click

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -2,10 +2,19 @@ import PropTypes from "prop-types";
 
 export const CounterApp = ( {value} ) => {
 
+  const handleAdd = ( value) => {
+    console.log(+1);
+    value = 1000;
+    console.log(value);
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { value } </h2>
+      <button onClick={ handleAdd }>
+        +1
+      </button>
     </>
   )
 
@@ -14,4 +23,3 @@ export const CounterApp = ( {value} ) => {
 CounterApp.protsTypes = {
   value: PropTypes.number.isRequired,
 }
-
