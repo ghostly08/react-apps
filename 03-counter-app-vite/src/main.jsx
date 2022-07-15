9bbddc440cee64785680ad2d2b230407f6c85790
ghostly08 <steven.produccionbcn@gmail.com>
2022-07-15 12:27:04 +0200

    Counter App exercice

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
+// :::::::::::: Import Functional Components ::::::::::: //
+
 import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+
+// ::::::::::: Import CSS file ::::::::::::: //
 
 import './styles.css';
 
+
+// :::::::::::: Rendering in the DOM :::::::::::: //
+
 ReactDOM.createRoot(document.getElementById('root')).render (
   <React.StrictMode>
-    <HelloWorldApp/> {/*Hago l allamada a la function App()*/}
-    <FirstApp/>
+    <CounterApp value={1234}/>
+    {/* <HelloWorldApp/> {/*Hago l allamada a la function App()*/}
+    {/* <FirstApp/> */}
   </React.StrictMode>
 );
